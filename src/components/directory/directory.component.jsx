aa8a9b60d150a7114501419e24b5316f463384a8
daverussell13 <daverussell1309@gmail.com>
2021-09-10 21:18:54 +0700

    Add animation on hover menu-items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,28 +9,28 @@ class Directory extends Component {
     this.state = {
       sections: [
         {
-          title: 'HATS',
+          title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1
         },
         {
-          title: 'JACKETS',
+          title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2
         },
         {
-          title: 'SNEAKERS',
+          title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3
         },
         {
-          title: 'WOMENS',
+          title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4
         },
         {
-          title: 'MENS',
+          title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5
